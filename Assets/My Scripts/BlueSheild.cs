220121e93358c1806ee316a0e5e5defc71efe956
cgodden18 <cgodden18@blakeschool.org>
2017-01-17 12:58:08 -0600

    did shit

---
+++ Assets/My Scripts/BlueSheild.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueSheild : MonoBehaviour 
+{
+	public GameObject Sheild;
+
+	private float SheildAmount;
+
+	void Start ()
+	{
+		SheildAmount = 10;
+	}
+
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.Q) && SheildAmount > 0);
+			{
+				SheildAmount -= 1;
+				UseSheild ();
+			}
+	}
+
+	void UseSheild ()
+	{
+				
+	}
+
+}
